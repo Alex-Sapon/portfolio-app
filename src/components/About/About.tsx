829b01ed4916411cf0f6eea8e42da9c61d85f13c
Aleksandr_Saponchik <alexsapon@gmail.com>
2022-05-10 18:25:06 +0300

    style: add some style to About

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,8 +6,18 @@ export const About = () => {
     return (
         <div className={`${styles.about_container} ${mainStyles.main_container}`}>
             <h2 className={styles.about_title}>About me</h2>
-            <div className={styles.about_items}>
-                {[...Array(6)].map((item, i) => <Item key={i}/>)}
+            <div className={styles.about_section}>
+                <h3 className={styles.about_subtitle}>Education</h3>
+                <div className={styles.about_items}>
+                    {[...Array(3)].map((item, i) => <Item key={i}/>)}
+                </div>
+            </div>
+
+            <div>
+                <h3 className={styles.about_subtitle}>Career</h3>
+                <div className={styles.about_items}>
+                    {[...Array(3)].map((item, i) => <Item key={i}/>)}
+                </div>
             </div>
         </div>
     )
--- src/components/About/Item/Item.tsx
+++ src/components/About/Item/Item.tsx
@@ -1,9 +1,20 @@
 import styles from './Item.module.css';
+import BG from '../../../common/images/bg.png'
 
 export const Item = () => {
     return (
         <div className={styles.item_container}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab doloremque earum sequi, beatae id dolores adipisci ducimus saepe illo, nihil hic natus. Impedit soluta molestias consectetur id, dolor doloribus eaque.
+            <div className={styles.item_image}>
+                <img src={BG} alt=""/>
+            </div>
+            <div className={styles.item_description}>
+                <h4 className={styles.item_title}>National University of Computer and Emerging Sciences</h4>
+                <p className={styles.item_subtitle}>Bachelor's Degree in Computer Science</p>
+                <div className={styles.item_skill}>
+                    <span>economist-manager</span>
+                </div>
+            </div>
+            <div className={styles.item_year}>2012 - 2017</div>
         </div>
     )
 }
